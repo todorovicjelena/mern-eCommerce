a72480a9aa91c808e07e3fd7aab70ed0ffc0fc39
Jelena Todorovic <todorovicjelenajeka@gmail.com>
2020-12-05 21:11:43 +0100

    product details screen

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; eCommerce</Col>
+          <Col className='text-center py-3'>
+            Copyright &copy; Jelena Todorovic
+          </Col>
         </Row>
       </Container>
     </footer>
